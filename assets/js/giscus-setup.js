cdb59770c6e2a98244d8655c077d12e698c881f8
QChuck <hyeongrak.choi@stonybrook.edu>
2026-01-15 21:32:19 +0000

    Deploying to gh-pages from @ QEP-StonyBrook/QEP-StonyBrook.github.io@0a9522e614df42bf196ae505447c25a9f42272dc 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "QEP-StonyBrook/QEP-StonyBrook",
+    "data-repo": "QEP-StonyBrook/QEP-StonyBrook.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
